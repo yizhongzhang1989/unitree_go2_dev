3ad756cc3a59fe9f32b4c615e8875f7936e97fd3
Yizhong Zhang <yizhongzhang1989@gmail.com>
2026-03-03 12:07:33 +0800

    integrated all status into control web

--- colcon_ws/src/low_level_joint_control_web/setup.py
+++ colcon_ws/src/low_level_joint_control_web/setup.py
@@ -6,6 +6,9 @@ setup(
     name=package_name,
     version='0.1.0',
     packages=find_packages(exclude=['test']),
+    package_data={
+        package_name: ['static/*.html'],
+    },
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
